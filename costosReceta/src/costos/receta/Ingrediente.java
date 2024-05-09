6a8702c84bc20330c59440b637a5c0947fad9750
PCesar45 <pacviu@gmail.com>
2024-05-08 22:22:51 -0600

    Aplicacion casi lista solo falta eliminar de la receta y de la lista y validaciones

---
+++ costosReceta/src/costos/receta/Ingrediente.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package costos.receta;
+
+/**
+ *
+ * @author Pablo
+ */
+public class Ingrediente {
+    private String nombre;
+    private double costoPorUnidad; // Costo por unidad
+    private String unidad;
+
+    public Ingrediente() {
+    }
+    
+    public Ingrediente(String nombre, double costoPorUnidad,String unidad) {
+        this.nombre = nombre;
+        this.costoPorUnidad = costoPorUnidad;
+        this.unidad=unidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getCostoPorUnidad() {
+        return costoPorUnidad;
+    }
+
+    public void setCostoPorUnidad(double costoPorUnidad) {
+        this.costoPorUnidad = costoPorUnidad;
+    }
+    
+  
+
+    public String getUnidad() {
+        return unidad;
+    }
+
+    public void setUnidad(String unidad) {
+        this.unidad = unidad;
+    }
+    
+    
+}
